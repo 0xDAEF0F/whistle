72562529bf51647060070f847ff8a52d17e8dfac
Alex T <aletapia@proton.me>
2025-03-07 13:15:25 -0700

    feat: implement keyboard shortcut handling and enhance recording transcription with language support

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,6 +2,7 @@
 
 mod constants;
 use anyhow::{Context, Result};
+use chrono::naive;
 use constants::API_URL;
 use cpal::{
     Stream,
@@ -12,6 +13,7 @@ use hound::{WavSpec, WavWriter};
 use serde::Deserialize;
 use std::{
     cell::RefCell,
+    collections::HashSet,
     sync::{Arc, Mutex, RwLock},
     time::Duration,
 };
@@ -170,6 +172,32 @@ impl AudioRecorder {
     }
 }
 
+#[derive(Clone, PartialEq, Eq, Default, Debug)]
+pub struct KeyboardShortcut {
+    modifiers: HashSet<Keycode>,
+    action_key: Option<Keycode>,
+}
+
+impl KeyboardShortcut {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn add_key(&mut self, key: Keycode) {
+        match key {
+            Keycode::Command | Keycode::LOption | Keycode::ROption => {
+                self.modifiers.insert(key);
+            }
+            _ => self.action_key = Some(key),
+        }
+    }
+}
+
+pub enum ShortcutAction {
+    ToggleRecording,
+    ToggleRecordingSpanish,
+}
+
 thread_local! {
     static RECORDER: RefCell<AudioRecorder> = RefCell::new(AudioRecorder::new());
 }
@@ -198,8 +226,13 @@ pub fn main() {
             Ok(())
         })
         .on_tray_icon_event(|app_handle, event| {
-            if let Err(e) = tray_event_handler(app_handle, event) {
-                log::error!("Error handling tray event: {}", e);
+            if let TrayIconEvent::Click {
+                id: tray_id,
+                button_state: MouseButtonState::Down,
+                ..
+            } = event
+            {
+                spawn(toggle_recording(app_handle.app_handle().clone(), tray_id));
             }
         })
         .plugin(tauri_plugin_clipboard_manager::init())
@@ -338,97 +371,81 @@ async fn key_logger(app_handle: AppHandle, tray_id: TrayIconId) -> Result<()> {
     }
 }
 
-fn tray_event_handler(app_handle: &AppHandle, event: TrayIconEvent) -> Result<()> {
-    if let TrayIconEvent::Click {
-        id: tray_id,
-        button_state: MouseButtonState::Down,
-        ..
-    } = event
-    {
-        let is_recording = RECORDER.with(|recorder| recorder.borrow().is_recording);
-        match is_recording {
-            false => {
-                _ = RECORDER.with(|recorder| {
-                    let tray_icon =
-                        app_handle.tray_by_id(&tray_id).with_context(|| {
-                            format!("could not get tray_icon from tray_id: {tray_id:?}")
-                        })?;
+/// This function:
+/// - Changes the tray icon.
+/// - Pauses Spotify if it is playing.
+/// - Starts recording or stops recording.
+/// - Calls the API to transcribe the recording.
+async fn toggle_recording(app_handle: AppHandle, tray_id: TrayIconId) -> Result<()> {
+    let tray_icon = app_handle
+        .tray_by_id(&tray_id)
+        .with_context(|| format!("could not get tray_icon from tray_id: {tray_id:?}"))?;
 
-                    // TODO: can we check if Spotify is running first?
-                    let _ = std::process::Command::new("osascript")
-                        .args(["-e", "tell application \"Spotify\" to pause"])
-                        .output();
+    let is_recording = RECORDER.with_borrow(|recorder| recorder.is_recording);
 
-                    recorder.borrow_mut().start_recording();
-                    tray_icon
-                        .set_icon(Some(Image::from_path("icons/recording-icon.png")?))?;
+    if !is_recording {
+        return RECORDER.with_borrow_mut(|recorder| {
+            _ = std::process::Command::new("osascript")
+                .args(["-e", "tell application \"Spotify\" to pause"])
+                .output();
 
-                    anyhow::Ok(())
-                });
-            }
-            true => {
-                let recording_bytes = RECORDER.with(|recorder| {
-                    let mut recorder = recorder.borrow_mut();
-                    recorder
-                        .stop_recording_and_get_bytes()
-                        .context("Failed to stop recording")
-                })?;
-                let tray_icon = app_handle.tray_by_id(&tray_id).with_context(|| {
-                    format!("could not get tray_icon from tray_id: {tray_id:?}")
-                })?;
-                tray_icon
-                    .set_icon(Some(Image::from_path("icons/transcribing-icon.png")?))?;
+            recorder.start_recording();
 
-                #[derive(Deserialize)]
-                struct Response {
-                    text: String,
-                }
+            tray_icon.set_icon(Some(Image::from_path("icons/recording-icon.png")?))?;
 
-                let app_handle = app_handle.clone();
-                tauri::async_runtime::spawn(async move {
-                    async fn call_api_and_retrieve_transcription(
-                        http_client: State<'_, reqwest::Client>,
-                        recording: Vec<u8>,
-                    ) -> Result<String> {
-                        let res = http_client
-                            .post(API_URL)
-                            .header("Content-Type", "audio/wav")
-                            .body(recording)
-                            .send()
-                            .await?;
-
-                        let response: Response =
-                            serde_json::from_str(&res.text().await?)?;
-
-                        Ok(response.text)
-                    }
-
-                    let transcription = call_api_and_retrieve_transcription(
-                        app_handle.state::<reqwest::Client>(),
-                        recording_bytes,
-                    )
-                    .await;
-
-                    match transcription {
-                        std::result::Result::Ok(text) => {
-                            log::info!("Transcription success: {}", text);
-                            app_handle.clipboard().write_text(text)?;
-                            tray_icon
-                                .set_icon(Some(Image::from_path("icons/icon.png")?))?;
-                        }
-                        Err(e) => {
-                            log::error!("Error writing to clipboard: {}", e);
-                            tray_icon
-                                .set_icon(Some(Image::from_path("icons/icon.png")?))?;
-                            let default_icon = app_handle.default_window_icon().unwrap();
-                            tray_icon.set_icon(Some(default_icon.clone()))?;
-                        }
-                    }
-                    anyhow::Ok(())
-                });
-            }
-        }
+            Ok(())
+        });
     }
 
+    let recording_bytes = RECORDER
+        .with_borrow_mut(|recorder| recorder.stop_recording_and_get_bytes())
+        .context("Failed to stop recording")?;
+
+    let transcription = call_api_and_retrieve_transcription(
+        app_handle.state::<reqwest::Client>(),
+        recording_bytes,
+        None,
+    )
+    .await;
+
+    match transcription {
+        Ok(text) => {
+            log::info!("Transcription success: {}", text);
+            app_handle.clipboard().write_text(text)?;
+            tray_icon.set_icon(Some(Image::from_path("icons/icon.png")?))?;
+        }
+        Err(e) => {
+            log::error!("Error writing to clipboard: {}", e);
+            tray_icon.set_icon(Some(Image::from_path("icons/icon.png")?))?;
+            let default_icon = app_handle.default_window_icon().unwrap();
+            tray_icon.set_icon(Some(default_icon.clone()))?;
+        }
+    }
     Ok(())
+}
+
+async fn call_api_and_retrieve_transcription(
+    http_client: State<'_, reqwest::Client>,
+    recording: Vec<u8>,
+    language: Option<&str>, // English or Spanish (default English)
+) -> Result<String> {
+    let lang = language.unwrap_or("English");
+    if lang != "English" && lang != "Spanish" {
+        anyhow::bail!("Invalid language!");
+    }
+    let res = http_client
+        .post(API_URL)
+        .header("Content-Type", "audio/wav")
+        .query(&["lang", lang])
+        .body(recording)
+        .send()
+        .await?;
+
+    #[derive(Deserialize)]
+    struct Response {
+        text: String,
+    }
+    let response: Response = serde_json::from_str(&res.text().await?)?;
+
+    Ok(response.text)
 }
