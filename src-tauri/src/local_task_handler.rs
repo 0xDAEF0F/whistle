b04a8ba9ca3d0ac51ce641217768902284d53d7d
Alex T <aletapia@proton.me>
2025-03-16 10:55:06 -0600

    feat: add spotify auto-pause during voice recording

--- src-tauri/src/local_task_handler.rs
+++ src-tauri/src/local_task_handler.rs
@@ -7,7 +7,6 @@ use anyhow::Result;
 use std::{cell::RefCell, rc::Rc};
 use tauri::{AppHandle, Manager};
 use tokio::{
-    process,
     sync::{mpsc, oneshot},
     task::LocalSet,
 };
@@ -31,18 +30,20 @@ pub fn run_local_task_handler(mut rx: mpsc::Receiver<Task>) {
             EnigoInstance::try_new().expect("Failed to create EnigoInstance"),
         ));
         let audio_recorder = Rc::new(RefCell::new(AudioRecorder::new()));
+        let media_player = Rc::new(RefCell::new(MediaPlayer::new()));
         while let Some(task) = rx.recv().await {
             let enigo = Rc::clone(&enigo);
             let audio_recorder = Rc::clone(&audio_recorder);
+            let media_player = Rc::clone(&media_player);
             tokio::task::spawn_local(async move {
                 match task {
                     Task::ToggleRecording(tx_recording, app_handle) => {
-                        pause_spotify().await.unwrap();
-
                         let mut recorder = audio_recorder.borrow_mut();
+                        let mut media_player = media_player.borrow_mut();
                         let transcribe_icon = app_handle.state::<TranscribeIcon>();
 
                         if !recorder.is_recording {
+                            media_player.pause_spotify().unwrap();
                             transcribe_icon.change_icon(Icon::Recording).unwrap();
                             recorder.start_recording();
                             _ = tx_recording.send(vec![]);
@@ -56,6 +57,7 @@ pub fn run_local_task_handler(mut rx: mpsc::Receiver<Task>) {
                             log::error!("Failed to stop recording");
                             return;
                         };
+                        media_player.play_spotify().unwrap();
 
                         if tx_recording.send(recording_bytes).is_err() {
                             log::error!("Failed to send recording to channel");
@@ -75,20 +77,47 @@ pub fn run_local_task_handler(mut rx: mpsc::Receiver<Task>) {
     rt.block_on(local);
 }
 
-async fn pause_spotify() -> Result<()> {
-    let output = process::Command::new("osascript")
-        .args(["-e", "tell application \"System Events\" to (name of processes) contains \"Spotify\""])
-        .output()
-        .await?;
+struct MediaPlayer {
+    was_playing: bool,
+}
 
-    let is_running = String::from_utf8(output.stdout)?.trim() == "true";
-
-    if is_running {
-        process::Command::new("osascript")
-            .args(["-e", "tell application \"Spotify\" to pause"])
-            .output()
-            .await?;
+impl MediaPlayer {
+    fn new() -> Self {
+        Self { was_playing: false }
     }
 
-    Ok(())
+    fn pause_spotify(&mut self) -> Result<()> {
+        let output = std::process::Command::new("osascript")
+        .args(["-e", "tell application \"System Events\" to (name of processes) contains \"Spotify\""])
+        .output()?;
+        let is_running = String::from_utf8(output.stdout)?.trim() == "true";
+
+        let output = std::process::Command::new("osascript")
+            .args(["-e", "tell application \"Spotify\" to player state"])
+            .output()?;
+        let is_playing = String::from_utf8(output.stdout)?.trim() == "playing";
+
+        if is_running && is_playing {
+            std::process::Command::new("osascript")
+                .args(["-e", "tell application \"Spotify\" to pause"])
+                .output()?;
+            self.was_playing = true;
+        }
+
+        Ok(())
+    }
+
+    fn play_spotify(&mut self) -> Result<()> {
+        if !self.was_playing {
+            return Ok(());
+        }
+
+        std::process::Command::new("osascript")
+            .args(["-e", "tell application \"Spotify\" to play"])
+            .output()?;
+
+        self.was_playing = false;
+
+        Ok(())
+    }
 }
--- todos.md
+++ todos.md
@@ -2,6 +2,7 @@
 
 - [ ] Configure a max duration for the recording.
 - [ ] Add a sound/popup to notify when transcription is done and in clipboard.
-- [ ] If music was playing, resume it after transcription.
+- [ ] Support other media players other than Spotify.
 - [ ] Add panel to customize hotkeys.
 - [ ] Add idle logo/image to tray icon.
+- [ ] Make a detailed monitoring with tokio to analyze performance.
